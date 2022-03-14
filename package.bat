6822e162ef867fca99c0b426025f5cc72a5467a2
matobataketoshi <54569734+matobataketoshi@users.noreply.github.com>
2022-03-14 20:29:27 +0900

    Update package.bat

--- package.bat
+++ package.bat
@@ -1,22 +1,22 @@
 set GOOS=darwin
 set GOARCH=amd64
-go build -o ./bin/colab-dlshogi-gd-for-mac 
+go build -o ./bin/colab-stockfish-gd-for-mac 
 
 set GOOS=linux
 set GOARCH=amd64
-go build -o ./bin/colab-dlshogi-gd-for-linux
+go build -o ./bin/colab-stockfish-gd-for-linux
 
 set GOOS=windows
 set GOARCH=amd64
-go build -o ./bin/colab-dlshogi-gd-for-windows
+go build -o ./bin/colab-stockfish-gd-for-windows
 
 pushd bin
 del /q *.zip
-copy /y colab-dlshogi-gd-for-mac colab-dlshogi-gd
-tar -a -c -f colab-dlshogi-gd.mac.zip colab-dlshogi-gd
-copy /y colab-dlshogi-gd-for-linux colab-dlshogi-gd
-tar -a -c -f colab-dlshogi-gd.linux.zip colab-dlshogi-gd
-copy /y colab-dlshogi-gd-for-windows colab-dlshogi-gd.exe
-tar -a -c -f colab-dlshogi-gd.windows.zip colab-dlshogi-gd.exe
-del /q colab-dlshogi-gd colab-dlshogi-gd.exe
+copy /y colab-stockfish-gd-for-mac colab-stockfish-gd
+tar -a -c -f colab-stockfish-gd.mac.zip colab-stockfish-gd
+copy /y colab-stockfish-gd-for-linux colab-stockfish-gd
+tar -a -c -f colab-stockfish-gd.linux.zip colab-stockfish-gd
+copy /y colab-stockfish-gd-for-windows colab-stockfish-gd.exe
+tar -a -c -f colab-stockfish-gd.windows.zip colab-stockfish-gd.exe
+del /q colab-stockfish-gd colab-stockfish-gd.exe
 popd
