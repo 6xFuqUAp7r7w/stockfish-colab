f6d174cd870c6d9c323789d3eda3c647e92851ba
matobataketoshi <54569734+matobataketoshi@users.noreply.github.com>
2022-03-14 20:27:56 +0900

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-GOOS=darwin GOARCH=amd64 go build -o ./bin/colab-dlshogi-gd-for-mac 
-GOOS=linux GOARCH=amd64 go build -o ./bin/colab-dlshogi-gd-for-linux
-GOOS=windows GOARCH=amd64 go build -o ./bin/colab-dlshogi-gd-for-windows
+GOOS=darwin GOARCH=amd64 go build -o ./bin/colab-stockfish-gd-for-mac 
+GOOS=linux GOARCH=amd64 go build -o ./bin/colab-stockfish-gd-for-linux
+GOOS=windows GOARCH=amd64 go build -o ./bin/colab-stockfish-gd-for-windows
 
 cd bin
 rm -rf *.zip
-cp colab-dlshogi-gd-for-mac colab-dlshogi-gd
-zip colab-dlshogi-gd.mac.zip colab-dlshogi-gd
-cp colab-dlshogi-gd-for-linux colab-dlshogi-gd
-zip colab-dlshogi-gd.linux.zip colab-dlshogi-gd
-cp colab-dlshogi-gd-for-windows colab-dlshogi-gd.exe
-zip colab-dlshogi-gd.windows.zip colab-dlshogi-gd.exe
-rm colab-dlshogi-gd colab-dlshogi-gd.exe
+cp colab-stockfish-gd-for-mac colab-stockfish-gd
+zip colab-stockfish-gd.mac.zip colab-stockfish-gd
+cp colab-stockfish-gd-for-linux colab-stockfish-gd
+zip colab-stockfish-gd.linux.zip colab-stockfish-gd
+cp colab-stockfish-gd-for-windows colab-stockfish-gd.exe
+zip colab-stockfish-gd.windows.zip colab-stockfish-gd.exe
+rm colab-stockfish-gd colab-stockfish-gd.exe
 cd -
